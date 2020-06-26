1962dd82ff4df813cca7f5339260a67ca3a2ab77
Lucas Tindall <ltindall@ucsd.edu>
2020-06-26 14:48:42 -0700

    added setup.py install file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='craft',
+      version='0.0.1',
+      description='CRAFT text detection',
+      url='https://github.com/ltindall/craft',
+      packages=['craft'],
+)
